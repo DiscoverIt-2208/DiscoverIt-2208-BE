b905fff9e7d107222be463f3a718d0948aa1ec80
Lucas Colwell <colwellc37@gmail.com>
2023-02-05 14:24:05 -0700

    Change dummy test to circle ci on PR

--- spec/dummy_test_spec.rb
+++ spec/dummy_test_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'dummy_test' do
-  context 'circleCI initial set-up' do  
-    it 'checks string is string' do   
-      expect("this is a test").to be_a(String)
+  context 'circleCI initial set-up' do
+    it 'checks string is string' do
+      expect("this is a test").to be_a(Integer)
     end
   end
 end
