b336762b01406344035c3adb3895c04ec7ea2efe
Mostafa Sakr <msakr3@gmail.com>
2023-02-06 19:04:51 -0700

    Add rack-cors gem and modify config/initializers/cors.rb file to allow FE to query.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create(name: 'Ash Ketchup', email: 'ash_ketchup@pokemon.com', password: 'derpderpderp')
+User.create(name: 'Mega Man', email: 'mega_man@legends.com', password: 'whatever')
