8f345ce0c615406593b8bf6bd9c003a0c9ece55d
Lucas Colwell <colwellc37@gmail.com>
2023-02-08 15:42:27 -0700

    Test services/facades

---
+++ spec/facades/places_facade_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe PlacesFacade do #, :vcr do
+  it 'returns places from capital of searched country' do
+    city = "Paris"
+    country = "France"
+
+    city_info = GeocodingFacade.city_info(city, country)
+    hotels = PlacesFacade.hotels(city_info)
+    # require "pry"; binding.pry
+
+    expect(hotels).to be_an Array
+    hotels.each do |h|
+      expect(h).to be_a Hotel
+    end
+  end
+end
---
+++ spec/services/geocoding_service_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe GeocodingService do #, :vcr do
+    it '.capital_info' do
+      city_name = "Paris"
+      country = "France"
+        response = GeocodingService.city_info(city_name, country)
+        # require "pry"; binding.pry
+        expect(response).to be_a Array
+        expect(response.first).to be_a Hash
+        expect(response.first).to have_key :name
+        expect(response.first).to have_key :latitude
+        expect(response.first).to have_key :longitude
+        expect(response.first).to have_key :country
+    end
+end
