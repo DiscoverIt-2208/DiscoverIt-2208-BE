c18be07e689326811139eab50e871663d5906d05
Mostafa Sakr <msakr3@gmail.com>
2023-02-10 14:48:55 -0700

    Fix a modification in places_service unintentionally included in this PR
    Co-authored-by: Ashley Turner <105073232+ashuhleyt@users.noreply.github.com>
    Co-authored-by: Lucas Colwell <98673086+lcole37@users.noreply.github.com>
    Co-authored-by: Astrid Hecht <106942456+Astrid-Hecht@users.noreply.github.com>

--- app/services/places_service.rb
+++ app/services/places_service.rb
@@ -3,9 +3,9 @@ class PlacesService
     Faraday.new(url: 'https://api.geoapify.com', params: { apiKey: ENV['places_api_key']})
   end
 
-  default_categories = []
+  # default_categories = string
 
-  def self.get_hotels(city_info, caetgory = default_categories)
+  def self.get_hotels(city_info)
     response = conn.get("v2/places?categories=accommodation.hotel&filter=circle:#{city_info.longitude},#{city_info.latitude},20000&limit=20&offset=0")
     parse = JSON.parse(response.body, symbolize_names: true)[:features]
   end
