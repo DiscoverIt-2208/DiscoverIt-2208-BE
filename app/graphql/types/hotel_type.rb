19697ee786cc6acc24117ff053f487ff54998128
Mostafa Sakr <msakr3@gmail.com>
2023-02-10 17:26:43 -0700

    Set up graphql on new branch with none of the websocket dependencies for deployment

---
+++ app/graphql/types/hotel_type.rb
@@ -0,0 +1,9 @@
+module Types
+  class HotelType < Types::BaseObject
+    field :name, String
+    field :address, String
+    field :place_id, String
+    field :city, String
+    field :country, String
+  end
+end
---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,16 @@
+module Types
+  class QueryType < Types::BaseObject
+    include GraphQL::Types::Relay::HasNodeField
+    include GraphQL::Types::Relay::HasNodesField
+
+    field :user, Types::UserType, null: false do
+      argument :id, ID, required: true
+    end
+
+    def user(id:)
+      User.find(id)
+    end
+
+    field :hotels, resolver: Queries::FetchHotels
+  end
+end
