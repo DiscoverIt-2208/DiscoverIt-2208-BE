6846fb70657da58a4e17dc6afe7c49be92c78b98
Mostafa Sakr <msakr3@gmail.com>
2023-02-14 16:40:10 -0700

    Merge db changes from deployment to main.

---
+++ db/migrate/20230214031431_add_columns_to_favorites.rb
@@ -0,0 +1,8 @@
+class AddColumnsToFavorites < ActiveRecord::Migration[5.2]
+  def change
+    add_column :favorites, :city, :string
+    add_column :favorites, :country, :string
+    add_column :favorites, :state, :string, default: ''
+    add_column :favorites, :address, :string
+  end
+end
