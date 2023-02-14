cfc55563330ab5f9cfbf354a5a660a6646023bf9
Astrid Hecht <silentaviary@gmail.com>
2023-02-13 20:25:09 -0700

    Fix: Move target fields to a class level variable

--- app/facades/places_facade.rb
+++ app/facades/places_facade.rb
@@ -1,4 +1,8 @@
 class PlacesFacade
+  TARGET_FIELDS = {
+    geoapify: %i[name address_line1 address_line2 categories place_id lon lat]
+  }.freeze
+
   def self.places(city_info, categories = nil, page = 0)
     geoapify_get_places(city_info, categories, page)
   end
@@ -6,24 +10,24 @@ class PlacesFacade
   # PRIVATE METHODS
   #============================================================================
 
-  def self.search_mapper(places, target_fields)
+  def self.search_mapper(places, target_fields_key)
     places.map! do |hit|
-      hit.select { |key, _value| target_fields.include?(key) }
+      hit.select { |key, _value| TARGET_FIELDS[target_fields_key].include?(key) }
     end
   end
 
   #  Geoapify specific methods:
   #  -------------------------------------------------------------------------
 
-    # Manager method
+  # Manager method
   def self.geoapify_get_places(city_info, categories, page)
     places = geoapify_populate_results(city_info, categories, page)
     geoapify_filter(places)
   end
 
-    # Populates results until 20 are present. If a search finds <20, it increases the search radius and tries again.
-    # Accepts pagination, a page value is multiplied by 20 to get the correct result offset, but also adds the number of
-    # past results (if under 20) so results won't be repeated (hopefully).
+  # Populates results until 20 are present. If a search finds <20, it increases the search radius and tries again.
+  # Accepts pagination, a page value is multiplied by 20 to get the correct result offset, but also adds the number of
+  # past results (if under 20) so results won't be repeated (hopefully).
   def self.geoapify_populate_results(city_info, categories, page)
     places = []
     while places.count < 20
@@ -34,19 +38,17 @@ class PlacesFacade
     places.take(20)
   end
 
-    # Flattens hash and holds target fields for a call to the general mapping method. Use of other
-    # place DBs in the future will have their own custom method to allow search_mapper to format uniformly
+  # Flattens hash and holds target fields for a call to the general mapping method. Use of other
+  # place DBs in the future will have their own custom method to allow search_mapper to format uniformly
   def self.geoapify_filter(places)
-    target_fields = %i[name address_line1 address_line2 categories place_id lon lat]
-
     places.map! do |raw_hit|
       raw_hit[:properties]
     end
 
-    search_mapper(places, target_fields)
+    search_mapper(places, :geoapify)
   end
 
-    # END Geoapify methods
+  # END Geoapify methods
   #  -------------------------------------------------------------------------
 
   private_class_method :geoapify_get_places,
