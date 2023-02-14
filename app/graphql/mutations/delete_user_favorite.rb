b477d8f5b9900446d95334bdcb362a062f7e89f9
Mostafa Sakr <msakr3@gmail.com>
2023-02-14 16:30:59 -0700

    Modify delete mutation

--- app/graphql/mutations/delete_user_favorite.rb
+++ app/graphql/mutations/delete_user_favorite.rb
@@ -5,20 +5,13 @@ module Mutations
       argument :id, Integer, required: true
 
       field :success, String, null: true
-      field :error, String, null: true
-  
 
       def resolve(args)
-        if favorite = Favorite.find(args[:id])
-            favorite.destroy
-              { 
-                success: "#{args[:place_name]} has been successfully deleted!"
-              } 
-        else
-              {
-                error: "#{args[:place_name]} is not in favorites!"
-              }
-        end
+        favorite = Favorite.find(args[:id])
+        favorite.destroy
+           { 
+             success: "Favorite has been successfully deleted!"
+           } 
       end
     end
   end
