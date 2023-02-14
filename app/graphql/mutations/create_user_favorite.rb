003f92c2946b9aabd4bf97101acb4bc4e4a96d37
Mostafa Sakr <msakr3@gmail.com>
2023-02-14 16:38:23 -0700

    Merge graphql changes from deployment branch into main branch locally.

---
+++ app/graphql/mutations/create_user_favorite.rb
@@ -0,0 +1,49 @@
+module Mutations
+  class CreateUserFavorite < Mutations::BaseMutation
+    description "Create a favorite for a user"
+
+    argument :user_id, Integer, required: true
+    argument :ninja_id, String, required: true
+    argument :place_name, String, required: true
+    argument :thumbnail_url, String, required: true
+    argument :city, String, required: true
+    argument :state, String, required: false
+    argument :country, String, required: true
+    argument :address, String, required: true
+
+    field :success, String, null: true
+    field :error, String, null: true
+
+
+    def resolve(args)
+      if Favorite.find_by(user_id: args[:user_id],
+            ninja_id: args[:ninja_id],
+            place_name: args[:place_name],
+            thumbnail_url: args[:thumbnail_url],
+            city: args[:city],
+            state: args[:state],
+            country: args[:country],
+            address: args[:address]
+            )
+            { 
+              error: "#{args[:place_name]} is already in favorites!"
+            } 
+      else
+        Favorite.create(
+            user_id: args[:user_id],
+            ninja_id: args[:ninja_id],
+            place_name: args[:place_name],
+            thumbnail_url: args[:thumbnail_url],
+            city: args[:city],
+            state: args[:state],
+            country: args[:country],
+            address: args[:address]
+            )
+
+            {
+              success: "#{args[:place_name]} has been added to favorites!"
+            }
+      end
+    end
+  end
+end
