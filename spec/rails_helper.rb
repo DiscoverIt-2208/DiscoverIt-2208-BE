405b1604fd3eb111290466ed8ab91e00f66e7817
Astrid Hecht <silentaviary@gmail.com>
2023-02-15 10:59:15 -0700

    Test: Install and setup VCR

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -71,3 +71,13 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+  config.filter_sensitive_data('places_api_key') { ENV['places_api_key'] }
+  config.filter_sensitive_data('api_ninja_key') { ENV['api_ninja_key'] }
+  config.filter_sensitive_data('google_key') { ENV['google_key'] }
+  config.configure_rspec_metadata!
+  config.allow_http_connections_when_no_cassette = true
+end
