ed5fd42ffc332f78f6adb6816cf4aa9735d2f9ff
Astrid Hecht <silentaviary@gmail.com>
2023-02-15 10:34:17 -0700

    Fix: Add VCR ENV config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -75,7 +75,9 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
-  config.filter_sensitive_data('tmdb_key') { ENV['tmdb_key'] }
+  config.filter_sensitive_data('places_api_key') { ENV['places_api_key'] }
+  config.filter_sensitive_data('api_ninja_key') { ENV['api_ninja_key'] }
+  config.filter_sensitive_data('google_key') { ENV['google_key'] }
   config.configure_rspec_metadata!
   config.allow_http_connections_when_no_cassette = true
 end
