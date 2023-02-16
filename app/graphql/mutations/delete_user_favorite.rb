c1ef71ebd3d8e0276565895606e8751e409b2fb1
Mostafa Sakr <msakr3@gmail.com>
2023-02-15 17:44:15 -0700

    Fix delete mutation to work with FE set-up.

--- app/graphql/mutations/delete_user_favorite.rb
+++ app/graphql/mutations/delete_user_favorite.rb
@@ -2,12 +2,13 @@ module Mutations
     class DeleteUserFavorite < Mutations::BaseMutation
       description "Delete a favorite for a user"
   
-      argument :id, Integer, required: true
+      argument :place_id, String, required: true
+      argument :user_id, Integer, required: true
 
       field :success, String, null: true
 
       def resolve(args)
-        favorite = Favorite.find(args[:id])
+        favorite = Favorite.find_by(user_id: args[:user_id], place_id: args[:place_id])
         favorite.destroy
            { 
              success: "Favorite has been successfully deleted!"
