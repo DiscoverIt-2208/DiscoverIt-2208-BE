715cae2befa3c537bc95b17c2aaa4c2101c3faba
Astrid Hecht <silentaviary@gmail.com>
2023-02-16 19:41:01 -0700

    Feat: Landing search page uses google search instead of geoapify

--- app/services/google_service.rb
+++ app/services/google_service.rb
@@ -8,6 +8,16 @@ class GoogleService
     JSON.parse(response.body, symbolize_names: true)
   end
 
+  def self.get_city_places(city_info, page_token)
+    response = conn.get("place/textsearch/json") do |f|
+      f.params['query'] = "things to do in #{city_info[:name]}"
+      f.params['language'] = 'en'
+      f.params['location'] = "#{city_info[:latitude]},#{city_info[:longitude]}"
+      (f.params['pagetoken'] = page_token) if page_token.present?
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
   def self.get_photo_url(reference)
     response = conn.get('place/photo') do |f|
       f.params['photo_reference'] = reference
